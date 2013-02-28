05414a46f55b3e2f0caaf32d67e91dfcdd356a92
Shmuel Fomberg <shmuelfomberg@gmail.com>
2013-02-28 11:49:18 +0900

    getting files from github

--- extlib/WWW/Github/Files.pm
+++ extlib/WWW/Github/Files.pm
@@ -5,6 +5,7 @@ use LWP::UserAgent;
 use JSON qw{decode_json};
 use MIME::Base64 qw{decode_base64};
 use Data::Dumper;
+use Carp;
 
 sub new {
     my ($class, %options) = @_;
@@ -30,15 +31,9 @@ sub new {
 
 sub get_file {
     my ($self, $path) = @_;
-    my @path_list = split '/', $path;
-    die "files need to start with '/'"
-        unless @path_list > 1 and $path_list[0] eq '';
-    shift @path_list;
-    $self->__fetch_tree(undef, @path_list);
-    my $meta = $self->__get_file_meta($path);
-    return unless $meta;
-    return unless $meta->{type} eq 'blob';
-    my $f_data = $self->geturl('/git/blobs/'.$meta->{sha});
+    my $f_data = $self->get_file_meta($path);
+    croak("requested file is not a file! |$path|")
+        unless $f_data->{type} eq 'file';
     if ($f_data->{encoding} eq 'base64') {
         return decode_base64($f_data->{content});
     }
@@ -49,70 +44,41 @@ sub get_file {
 
 sub get_dir {
     my ($self, $path) = @_;
+    my $f_data = $self->get_file_meta($path);
+    croak('Are you sure $path is a directory?')
+        unless ref($f_data) eq 'ARRAY';
+    my @dirs = grep { 'dir' eq $_->{type} } @$f_data;
+    my @files = grep { 'file' eq $_->{type} } @$f_data;
+    return (\@dirs, \@files);
 }
 
 sub get_file_meta {
     my ($self, $path) = @_;
-}
-
-sub __get_file_meta {
-    my ($self, $path) = @_;
-    my @path_list = split '/', $path;
-    die "files need to start with '/'"
-        unless @path_list > 1 and $path_list[0] eq '';
-    shift @path_list;
-    my $dir_obj = $self->{root_dir};
-    while (my $filename = shift @path_list) {
-        my ($filedata) = grep { $filename eq $_->{path} } @{ $dir_obj->{content} };
-        return unless $filedata;
-        return if @path_list and $filedata->{type} ne 'tree';
-        $dir_obj = $filedata;
-    }
-    return $dir_obj;
+    croak("Path should start with '/'! |$path|")
+        unless $path =~ m!^/!;
+    my $commit = $self->__fetch_root();
+    my $f_data = $self->geturl("/contents$path?ref=$commit");
+    return $f_data;
 }
 
 sub refresh {
     my ($self) = @_;
 }
 
-sub __fetch_tree {
-    my ($self, $dir_obj, @path) = @_;
-    if (not $dir_obj) {
-        $dir_obj = $self->{root_dir} || $self->__fetch_root();
-    }
-    while (@path) {
-        if (not $dir_obj->{content}) {
-            my $data = $self->geturl('/git/trees/'.$dir_obj->{sha});
-            $dir_obj->{content} = $data->{tree};
-        }
-        my $filename = shift @path;
-        my ($filedata) = grep { $filename eq $_->{path} } @{ $dir_obj->{content} };
-        return unless $filedata;
-        last unless $filedata->{type} eq 'tree';
-        $dir_obj = $filedata;
-    }
-}
-
 sub __fetch_root {
     my $self = shift;
-    my $root;
+    my $root = $self->{root_commit};
+    return $root if $root;
+
     if ($self->{branch}) {
         my $b_data = $self->geturl('/branches/'.$self->{branch});
-        $root = {
-            commit => $b_data->{commit}->{sha},
-            sha => $b_data->{commit}->{commit}->{tree}->{sha},
-            type => 'tree',
-        }
+        $root = $b_data->{commit}->{sha};
     }
     else {
         my $c_data = $self->geturl('/git/commits/'.$self->{commit});
-        $root = {
-            commit => $c_data->{sha},
-            sha => $c_data->{tree}->{sha},
-            type => 'tree',
-        }
+        $root = $self->{commit};
     }
-    $self->{root_dir} = $root;
+    $self->{root_commit} = $root;
     return $root;
 }
 

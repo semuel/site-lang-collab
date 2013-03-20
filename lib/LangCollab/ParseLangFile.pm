7d0c7d0f75d976dd28d62adcc488c9daead77d13
Shmuel Fomberg <shmuelfomberg@gmail.com>
2013-03-20 22:20:40 +0900

    parsing lang files

--- lib/LangCollab/ParseLangFile.pm
+++ lib/LangCollab/ParseLangFile.pm
@@ -8,92 +8,94 @@ my $seperators = {
     '(' => ')',
 };
 
-sub extract_string {
+sub extract_space {
     my $rec = shift;
-    # $rec = { tokens => [], rest => 'sdfdff', buf => '' };
-    my @parts = split qr/([\'\"\#]|qq?.)/, $rec->{rest}, 2;
-    return unless scalar(@parts) == 3;
-    my $start = $rec->{buf};
-    $rec->{buf} = '';
-    $start .= shift @parts;
-    my $sep = shift @parts;
-    my $rest = shift @parts;
-
-    if ($sep eq '#') {
-        # a comment. eat the rest of the line
-        @parts = split qr/(\n)/, $rest, 2;
-        $rec->{rest} = pop @parts;
-        $rec->{buf} = $start . $sep . join('', @parts);
+    my ($space) = $rec->{rest} =~ m/^((?:,|\s|=>)*)/s;
+    if (length($space) > 0) {
+        push @{ $rec->{tokens} }, [ 'TEXT', $space ];
+        substr($rec->{rest}, 0, length($space), '');
         return 1;
     }
-    push @{ $rec->{tokens} }, [ 'TEXT', $start ];
-    $rec->{rest} = $rest;
+    return 0;
+}
 
-    my $endsep = $sep;
-    my $bk_sep;
-    if ($sep =~ /^qq?(.)$/) {
-        # we have either q{ or qq{
-        my $sepchar = $2;
-        if (exists $seperators->{$sepchar}) {
-            $endsep = $seperators->{$sepchar};
-            $bk_sep = $sepchar;
-        }
-        else {
-            $endsep = $sepchar;
-        }
+sub extract_comment {
+    my $rec = shift;
+    my ($comment) = $rec->{rest} =~ m/^#[^\n]*\n/;
+    if (length($comment) > 0) {
+        push @{ $rec->{tokens} }, [ 'COMMENT', $comment ];
+        substr($rec->{rest}, 0, length($comment), '');
+        return 1;
     }
+    die "failed to read comment";
+}
+
+sub extract_unquted_string {
+    my $rec = shift;
+    # this is unquoted string. 
+    my ($string) = $rec->{rest} =~ m/([A-Za-z]\w*)\s*=>/s;
+    die "failed to parse unquted string" unless defined $string;
+    push @{ $rec->{tokens} }, ['STR', $string, '', ''];
+    substr($rec->{rest}, 0, length($string), '');
+    return 1;
+}
+
+sub extract_quated_string {
+    my $rec = shift;
+    my $sep = substr($rec->{rest}, 0, 1, '');
     my $string = '';
-    if ($bk_sep) {
-        my $deep = 0;
-        while (1) {
-            my ($buf, $char);
-            ($buf, $char, $rest) = split /($endsep|$bk_sep)/, $rest, 2;
-            return unless defined $rest;
-            $string .= $buf;
-            my ($slashs) = $buf =~ m!(/*)$!;
-            if (length($slashs) % 2 == 1) {
-                # ah, we have spashed seperator. 
-                $string .= $char;
-                next;
-            }
-            elsif ($char eq $bk_sep) {
-                # a start baracks in the middle of the string
-                $deep++;
-                $string .= $char;
-                next;
-            }
-            elsif ($deep == 0) {
-                # end of string
-                last;
-            }
-            else {
-                # end of barack in the middle of the string
+    while (1) {
+        my ($start, $rest) = split $sep, $rec->{rest}, 2;
+        die "failed to parse quoted string" unless defined $rest;
+        $rec->{rest} = $rest;
+        $string .= $start;
+        my ($slashes) = $start =~ m/(\\*)$/;
+        if (length($slashes) % 2 == 1) {
+            # slashed seperator
+            $string .= $sep;
+            next;
+        }
+        push @{ $rec->{tokens} }, ['STR', $string, $sep, $sep];
+        return 1;
+    }
+}
+
+sub extract_quated_string_qq {
+    my $rec = shift;
+    my ($init, $sep) = $rec->{rest} =~ m/^(qq?)(.)/;
+    substr($rec->{rest}, 0, length($init) + length($sep), '');
+    my $endsep = exists $seperators->{$sep} ? $seperators->{$sep} : $sep;
+    my $string = '';
+    my $deep = 0;
+    while (1) {
+        my ($start, $char, $rest) = split qr/($sep|$endsep)/, $rec->{rest}, 2;
+        die "failed to parse quoted string" unless defined $rest;
+        $rec->{rest} = $rest;
+        $string .= $start;
+        my ($slashes) = $start =~ m/(\\*)$/;
+        if (length($slashes) % 2 == 1) {
+            # slashed seperator
+            $string .= $char;
+            next;
+        }
+        if ($char eq $sep) {
+            $deep++;
+            $string .= $char;
+            next;
+        }
+        if ($char eq $endsep) {
+            if ($deep > 0) {
                 $deep--;
                 $string .= $char;
                 next;
             }
-        }
-    }
-    else {
-        while (1) {
-            (my $buf, $rest) = split $endsep, $rest, 2;
-            return unless defined $rest;
-            $string .= $buf;
-            my ($slashs) = $buf =~ m!(/*)$!;
-            if (length($slashs) % 2 == 1) {
-                # ah, we have spashed end seperator. 
-                $string .= $endsep;
-                next;
-            }
             else {
-                # end of string
-                last;
+                push @{ $rec->{tokens} }, ['STR', $string, $init.$sep, $endsep];
+                return 1;
             }
         }
+        die "should not reach here";
     }
-    $rec->{rest} = $rest;
-    push @{ $rec->{tokens} }, ['STR', $string, $sep, $endsep];
-    return 1;
 }
 
 sub parse {
@@ -104,12 +106,29 @@ sub parse {
     my $start = join '', @parts;
     @parts = ();
     my $rec= { tokens => [], rest => $rest, buf => $start };
-    while ( extract_string( $rec ) ) {}
-    my $final = '';
-    $final .= $rec->{buf}  if defined $rec->{buf};
-    $final .= $rec->{rest} if defined $rec->{rest};
-    if (length($final) > 0) {
-        push @{ $rec->{tokens} }, ['TEXT', $final];
+    while (length($rec->{rest}) > 0) {
+        next if extract_space($rec);
+        if ( $rec->{rest} =~ m/^#/) {
+            extract_comment($rec);
+            next;
+        }
+        if ( $rec->{rest} =~ m/^('|")/ ) {
+            extract_quated_string($rec);
+            next;
+        }
+        if ( $rec->{rest} =~ m/^qq?./ ) {
+            extract_quated_string_qq($rec);
+            next;
+        }
+        if ( $rec->{rest} =~ m/^[A-Za-z]/) {
+            # probably unquoted string. i.e. word => 'val'
+            extract_unquted_string($rec);
+            next;
+        }
+        if ( $rec->{rest} =~ m/^\)/) {
+            # end of strings
+            last;
+        }
     }
     return $rec->{tokens};
 }

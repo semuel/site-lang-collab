aa2d5deef2a144bc918604e353a6ee389c794456
Shmuel Fomberg <shmuelfomberg@gmail.com>
2013-05-22 10:51:16 +0900

    testing and correcting ParseLangFile.pm

--- lib/LangCollab/ParseLangFile.pm
+++ lib/LangCollab/ParseLangFile.pm
@@ -21,7 +21,7 @@ sub extract_space {
 
 sub extract_comment {
     my $rec = shift;
-    my ($comment) = $rec->{rest} =~ m/^#[^\n]*\n/;
+    my ($comment) = $rec->{rest} =~ m/^(#[^\n]*\n)/;
     if (length($comment) > 0) {
         push @{ $rec->{tokens} }, [ 'COMMENT', $comment ];
         substr($rec->{rest}, 0, length($comment), '');
@@ -34,7 +34,8 @@ sub extract_unquted_string {
     my $rec = shift;
     # this is unquoted string. 
     my ($string) = $rec->{rest} =~ m/([A-Za-z]\w*)\s*=>/s;
-    die "failed to parse unquted string" unless defined $string;
+    die "failed to parse unquted string: |".substr($rec->{rest}, 0, 40)."|" 
+        unless defined $string;
     push @{ $rec->{tokens} }, ['STR', $string, '', ''];
     substr($rec->{rest}, 0, length($string), '');
     return 1;
@@ -64,11 +65,19 @@ sub extract_quated_string_qq {
     my $rec = shift;
     my ($init, $sep) = $rec->{rest} =~ m/^(qq?)(.)/;
     substr($rec->{rest}, 0, length($init) + length($sep), '');
-    my $endsep = exists $seperators->{$sep} ? $seperators->{$sep} : $sep;
+    my ($endsep, $search); 
+    if (exists $seperators->{$sep}) {
+        $endsep = $seperators->{$sep};
+        $search = "[\\$sep\\$endsep]";
+    }
+    else {
+        $search = quotemeta($sep);
+    }
+    $search = qr/($search)/;
     my $string = '';
     my $deep = 0;
     while (1) {
-        my ($start, $char, $rest) = split qr/($sep|$endsep)/, $rec->{rest}, 2;
+        my ($start, $char, $rest) = split $search, $rec->{rest}, 2;
         die "failed to parse quoted string" unless defined $rest;
         $rec->{rest} = $rest;
         $string .= $start;
@@ -78,21 +87,27 @@ sub extract_quated_string_qq {
             $string .= $char;
             next;
         }
-        if ($char eq $sep) {
-            $deep++;
-            $string .= $char;
-            next;
-        }
-        if ($char eq $endsep) {
-            if ($deep > 0) {
-                $deep--;
+        if (defined $endsep) {
+            if ($char eq $endsep) {
+                if ($deep > 0) {
+                    $deep--;
+                    $string .= $char;
+                    next;
+                }
+                else {
+                    push @{ $rec->{tokens} }, ['STR', $string, $init.$sep, $endsep];
+                    return 1;
+                }
+            }
+            if ($char eq $sep) {
+                $deep++;
                 $string .= $char;
                 next;
             }
-            else {
-                push @{ $rec->{tokens} }, ['STR', $string, $init.$sep, $endsep];
-                return 1;
-            }
+        }
+        elsif ($char eq $sep) {
+            push @{ $rec->{tokens} }, ['STR', $string, $init.$sep, $sep];
+            return 1;
         }
         die "should not reach here";
     }
@@ -131,6 +146,37 @@ sub parse {
         }
     }
     return $rec->{tokens};
+}
+
+my %pslash = (
+    n => "\n",
+    t => "\t",
+    r => "\r",
+    f => "\f",
+    );
+
+sub get_str_val {
+    my $token = shift;
+    return $token->[1] unless $token->[0] eq 'STR';
+    my $str = $token->[1];
+    $str =~ s/\\(.)/exists $pslash{$1} ? "\\".$1 : $1 /ge;
+    return $str;
+}
+
+sub get_str_inter {
+    my $token = shift;
+    return $token->[1] unless $token->[0] eq 'STR';
+    my $str = $token->[1];
+    if ($token->[2] =~ m/^"|qq.$/) {
+        $str =~ s/\\(.)/exists $pslash{$1} ? $pslash{$1} : $1 /ge;
+    }
+    elsif ($token->[2] =~ m/^q?([^q])$/) {
+        my $sep = $1;
+        my @list = ($sep, "\\");
+        push @list, $seperators->{$sep} if exists $seperators->{$sep};
+        $str =~ s/\\(.)/(grep { $1 eq $_ } @list) ? $1 : "\\".$1 /ge;
+    }
+    return $str;
 }
 
 1;
